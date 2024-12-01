3ece5a3d36e20bd8c624d92a277716570544eec2
Tom.DevTech <Tom.Helling1234@gmail.com>
2024-12-01 19:25:24 +0100

    Updated code

--- main.py
+++ main.py
@@ -1,1 +1,28 @@
-print("Hello World!")
+import os
+from openai import OpenAI
+from dotenv import load_dotenv
+
+def CreateResponse(Input: str):
+    client = OpenAI(
+    api_key=os.getenv("API_KEY"))
+
+    completion = client.chat.completions.create(
+    model="gpt-4o-mini",
+    messages=[
+        {"role": "system", "content": "You are a helpful assistant."},
+        {
+            "role": "user",
+            "content": Input,
+        }
+    ])
+    print(completion.choices[0].message.content)
+    CallAPI()
+
+def CallAPI():
+    print("How can I help you?")
+    Input = input()
+    CreateResponse(Input)
+
+if __name__ == "__main__":
+    load_dotenv(".env")
+    CallAPI()
