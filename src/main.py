b74c3c0eda16ebc067ddf507ea7db4b2baf08937
Tom.DevTech <Tom.Helling1234@gmail.com>
2024-12-02 11:48:20 +0100

    adpated code style

--- src/main.py
+++ src/main.py
@@ -2,12 +2,13 @@ from dotenv import load_dotenv
 from ollama import chat
 from ollama import ChatResponse
 
+
 def CreateResponse(Input: str):
     response: ChatResponse = chat(model='llama3.2', messages=[
-    {
-        'role': 'user',
-        'content': Input,
-    },])
+        {
+            'role': 'user',
+            'content': Input,
+        },])
     print(response.message.content)
 
 
@@ -16,7 +17,7 @@ def CallAPI():
     Input = input()
     CreateResponse(Input)
     CallAPI()
-    
+
 
 if __name__ == "__main__":
     load_dotenv(".env")
