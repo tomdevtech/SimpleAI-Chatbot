0cb20ff9b34780d5bd62160aefbbf53ad034b2a7
Tom.DevTech <Tom.Helling1234@gmail.com>
2024-12-02 15:18:18 +0100

    Updated code again

--- src/main.py
+++ src/main.py
@@ -1,24 +1,35 @@
 from dotenv import load_dotenv
-from ollama import chat
-from ollama import ChatResponse
+import os
+import json
+import httpx
 
 
-def CreateResponse(Input: str):
-    response: ChatResponse = chat(model='llama3.2', messages=[
-        {
-            'role': 'user',
-            'content': Input,
-        },])
-    print(response.message.content)
+def CreateResponse(Input: str, URL: str):
+    data = {
+        "model": "llama2",
+        "prompt": Input,
+    }
+    response = httpx.post(url=URL, data=json.dumps(data), headers={
+                          "Content-Type": "application/json"})
+
+    # Split the response by newlines and filter out empty lines
+    response_lines = [line for line in response.text.strip().split('\n') if line]
+
+    # Parse each line as JSON
+    response_dicts = [json.loads(line) for line in response_lines]
+
+    print(''.join(response_dict.get('response', '') for response_dict in response_dicts))
 
 
 def CallAPI():
-    CreateResponse("Tell me that you are an assistent and want to help me!")
+    print(os.getenv("URL"))
+    CreateResponse(
+        "Tell me that you are an assistent and want to help me!", os.getenv("URL"))
     Input = input()
-    CreateResponse(Input)
+    CreateResponse(Input, os.getenv("URL"))
     CallAPI()
 
 
 if __name__ == "__main__":
-    load_dotenv(".env")
+    load_dotenv("config/.env")
     CallAPI()
