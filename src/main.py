c27500d3c04295fa0b7a9b000675f538367d1b7c
Tom.DevTech <Tom.Helling1234@gmail.com>
2024-12-07 23:21:46 +0100

    Added basic Web UI with Streamlit

--- src/main.py
+++ src/main.py
@@ -1,60 +1,27 @@
 """This file runs the ai model and lets you interact with it."""
 
-from dotenv import load_dotenv
-from langchain_ollama import OllamaLLM
-from langchain_core.prompts import ChatPromptTemplate
+from ai import AIAssistant
+from ui import StreamlitUI
 
 
-class AIAssistent:
-    """AI Assistent Class."""
+class MainApp:
+    """Main Application class to combine AI Assistant and UI."""
 
-    def __init__(self, ModelName, Modelfile, Temperature):
-        """Initilization Process."""
-        self.ModelName = ModelName
-        self.ModelFile = Modelfile
-        self.Temperature = Temperature
-        self.Prompt = any
-        self.Chain = any
-        self.Context = ""
-        self.Template = (
-            f"Context: {self.ModelFile}"
-            + """
-        That`s the current conversation history: {context}
-        Answer to this question: {question}"""
+    def __init__(self):
+        """Initialize the application."""
+        self.AIAssistant = AIAssistant(
+            "llama3.2",
+            """You are a smart assistant called Bob and you know everything 
+            about computer science.""",
+            1.0
         )
+        self.UI = StreamlitUI(self.AIAssistant)
 
-    def CreateModel(self):
-        """Creation of the AI model with given specifications."""
-        self.Model = OllamaLLM(model=self.ModelName,
-                               temperature=self.Temperature)
-        self.Prompt = ChatPromptTemplate.from_template(self.Template)
-        self.Chain = self.Prompt | self.Model
-
-    def RunConversation(self):
-        """Run the AI model."""
-        print("Welcome to the AI Assistent! Type 'exit' to quit the program.")
-        while True:
-            UserInput = input("You: ")
-            if UserInput.lower() == "exit":
-                break
-            Result = self.Chain.invoke({"context": self.Context,
-                                        "question": UserInput})
-            print("AI Assistent: ", Result)
-            self.Context += f"\nUser: {UserInput}\nAI Assistent: {Result}"
-
-    def Main(self):
-        """Execute all methods."""
-        self.CreateModel()
-        self.RunConversation()
+    def Run(self):
+        """Run the application."""
+        self.UI.Run()
 
 
 if __name__ == "__main__":
-    load_dotenv("config/.env")
-    AI = AIAssistent(
-        "llama3.2",
-        """You are a smart assistent called Bob and you know
-        everything about computer science and want help to solve
-        every problem about that.""",
-        1.0,
-    )
-    AI.Main()
+    App = MainApp()
+    App.Run()
