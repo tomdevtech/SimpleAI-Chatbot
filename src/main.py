32aa371e768e97edf12e38502835663428afb3ea
Tom.DevTech <Tom.Helling1234@gmail.com>
2024-12-07 18:04:37 +0100

    updated errors

--- src/main.py
+++ src/main.py
@@ -37,7 +37,8 @@ class AIAssistent:
             UserInput = input("You: ")
             if UserInput.lower() == "exit":
                 break
-            Result = self.Chain.invoke({"context": self.Context, "question": UserInput})
+            Result = self.Chain.invoke({"context": self.Context, 
+                                        "question": UserInput})
             print("AI Assistent: ", Result)
             self.Context += f"\nUser: {UserInput}\nAI Assistent: {Result}"
 
