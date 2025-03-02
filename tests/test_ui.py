82fb368f208907316ad2806ddedf0a4e3e63b167
Tom.DevTech <Tom.Helling1234@gmail.com>
2025-03-02 17:23:18 +0100

    Adapted test for UI

--- tests/test_ui.py
+++ tests/test_ui.py
@@ -11,34 +11,17 @@ class TestUI:
     """Value Section."""
 
     @pytest.mark.parametrize(
-        """ModelName, Creativity, Prompt, SummaryPrompt, FileTypes,
-        expected_AIAssistant, expected_UI""",
+        """ expected_UI""",
         [
             (
-                "llama3.1:8b",
-                1,
-                "This is a test prompt.",
-                "This is a summary test prompt.",
-                [".py", ".js", ".java", ".md", ".txt"],
-                True,
                 True,
             ),
         ],
     )
     def test_UI(
         self,
-        ModelName,
-        Creativity,
-        Prompt,
-        SummaryPrompt,
-        FileTypes,
-        expected_AIAssistant,
         expected_UI,
     ):
         """Initialize the UI."""
-        self.AIAssistant = AIAssistant(
-            ModelName, Creativity, Prompt, SummaryPrompt, FileTypes
-        )
-        self.UI = StreamlitUI(self.AIAssistant)
-        assert (self.AIAssistant is not None) == expected_AIAssistant
+        self.UI = StreamlitUI()
         assert (self.UI is not None) == expected_UI
